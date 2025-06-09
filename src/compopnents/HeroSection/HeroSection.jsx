9f9efca64b7cc97c5a6a2b2362a9acbbbda609b1
kubra islami <kubra.islami.00@gmail.com>
2025-06-09 07:43:24 +0100

    worked on hero sectopm of page

--- src/compopnents/HeroSection/HeroSection.jsx
+++ src/compopnents/HeroSection/HeroSection.jsx
@@ -5,67 +5,93 @@ import "./HeroSection.css";
 function HeroSection() {
     return (
         <div className="hero-equal-height relative min-h-screen flex items-center text-white overflow-hidden">
-            {/* Background shape (left corner) */}
+            {/* Background shape */}
             <div
                 className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-left-top"
                 style={{backgroundImage: "url('/hero-section/t6-hero-shape 1.png')"}}
             ></div>
 
-            {/* Floating shapes */}
-            {/*<img*/}
-            {/*    src="/hero-section/cta-2-path-shape.png"*/}
-            {/*    alt="group shape"*/}
-            {/*    className="absolute top-60 left-60 w-10 md:w-10 z-10"*/}
-            {/*/>*/}
-            {/*<img*/}
-            {/*    src="/hero-section/cta-2-circle-shape.png"*/}
-            {/*    alt="group shape"*/}
-            {/*    className="absolute top-20 left-50 w-10 z-10"*/}
-            {/*/>*/}
-            {/*<img*/}
-            {/*    src="/hero-section/cta-2-tringle-shape.png"*/}
-            {/*    alt="group shape"*/}
-            {/*    className="absolute top-1/2 left-60 w-16 z-10"*/}
-            {/*/>*/}
-            {/*<img*/}
-            {/*    src="/hero-section/cta-2-tringle-shape.png"*/}
-            {/*    alt="group shape"*/}
-            {/*    className="absolute bottom-0 left-1/4 w-16 z-10"*/}
-            {/*/>*/}
-
-            {/* Content */}
-            <div className="container mx-auto px-4 relative z-20">
-                <div className="flex lg:flex-row items-center gap-20">
-                    <div className="w-full pt-12">
-                        <div className="hero-slider-content text-white">
-                            <strong className="text-secondary">Online Shop</strong>
-                            <h1 className="text-3xl md:text-5xl font-bold mt-4 text-white">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8 relative z-20 w-full">
+                <div className="flex lg:flex-row items-center gap-10 lg:gap-20">
+                    {/* Left Content */}
+                    <div className="w-full pt-8 lg:pt-12 lg:text-left flex items-center justify-center lg:justify-start text-content">
+                        <div className="hero-slider-content mx-auto lg:mx-0">
+                            <strong className="text-secondary block text-lg">Online Shop</strong>
+                            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mt-4 text-white leading-snug">
                                 Empower Your Business Traffic at The Top
                             </h1>
-                            <p className="text-lg mt-4" style={{maxWidth: "35rem"}}>
-                                Holistic procrastinate mission-critical convergence with reliable
-                                customer service. Assertively underwhelm idea-sharing for impactful solutions.
+                            <p className="text-base sm:text-lg mt-4 text-gray-200">
+                                Holistic procrastinate mission-critical convergence with reliable customer service.
+                                Assertively underwhelm idea-sharing for impactful solutions.
                             </p>
 
-                            <div className="mt-6">
+                            <div className="mt-6 get-start-btn">
                                 <Link
                                     to="#"
-                                    className="secondary-solid-btn inline-block bg-secondary text-white px-6 py-3 rounded-md hover:bg-secondary-dark transition">
+                                    className="secondary-solid-btn"
+                                >
                                     Get Start Now
                                 </Link>
                             </div>
                         </div>
-
                     </div>
-                    <div className="w-full pt-12">
+
+
+                    {/* Right Image */}
+                    <div className="w-full pt-8 lg:pt-12">
                         <img
                             src="/hero-section/hroo-img.png"
-                            alt="imgage of online-shop hero past. a girl gives some shopping pags"
-                            className="w-full object-contain h-dvh"
+                            alt="image of online-shop hero"
+                            className="img-hero w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto object-contain "
                         />
+
                     </div>
                 </div>
             </div>
+
+            {/* Floating shapes */
+            }
+            {/*<img*/
+            }
+            {/*    src="/hero-section/cta-2-path-shape.png"*/
+            }
+            {/*    alt="group shape"*/
+            }
+            {/*    className="absolute top-60 left-60 w-10 md:w-10 z-10"*/
+            }
+            {/*/>*/
+            }
+            {/*<img*/
+            }
+            {/*    src="/hero-section/cta-2-circle-shape.png"*/
+            }
+            {/*    alt="group shape"*/
+            }
+            {/*    className="absolute top-20 left-50 w-10 z-10"*/
+            }
+            {/*/>*/
+            }
+            {/*<img*/
+            }
+            {/*    src="/hero-section/cta-2-tringle-shape.png"*/
+            }
+            {/*    alt="group shape"*/
+            }
+            {/*    className="absolute top-1/2 left-60 w-16 z-10"*/
+            }
+            {/*/>*/
+            }
+            {/*<img*/
+            }
+            {/*    src="/hero-section/cta-2-tringle-shape.png"*/
+            }
+            {/*    alt="group shape"*/
+            }
+            {/*    className="absolute bottom-0 left-1/4 w-16 z-10"*/
+            }
+            {/*/>*/
+            }
+
         </div>
 
         // <div className="hero container-hero ">
